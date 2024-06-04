4e610193aae2c9f87deadaf1b28ac8eac7209918
pasnon lucas <107144928+warzskynox12@users.noreply.github.com>
2024-06-04 13:13:09 +0200

    update: route refaite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Children } from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import AuthPage from './AuthPage';
+import ProfilePage from './ProfilePage';
+import HomePage from './HomePage';
+
+const router = createBrowserRouter([
+  { 
+    path: '/nationglorycomu/', 
+    element: <HomePage />,
+    Children:[
+      { 
+        path: '/nationglorycomu/auth', 
+        element: <AuthPage />
+      },
+      { 
+        path: '/nationglorycomu/profile', 
+        element: <ProfilePage /> 
+      },
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

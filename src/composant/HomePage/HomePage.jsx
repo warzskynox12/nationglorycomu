a865e6861b1e7c99744cc52617d1f139fb7720e3
pasnon lucas <107144928+warzskynox12@users.noreply.github.com>
2024-06-21 20:32:32 +0200

    update : route

--- src/composant/HomePage/HomePage.jsx
+++ src/composant/HomePage/HomePage.jsx
@@ -5,25 +5,20 @@ import Navbar from '../Navbar/Navbar';
 
   
 const App = () => {
-  const [user, setUser] = useState(''); // État pour stocker les informations sur l'utilisateur
+  const [user, setUser] = useState('');
 
   useEffect(() => {
-      // Observer les changements d'état de l'utilisateur
       const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
           if (user) {
-              // Utilisateur connecté
-              setUser(user); // Mettre à jour l'état de l'utilisateur
+              setUser(user); 
           } else {
-              // Aucun utilisateur connecté
-              setUser(null); // Remettre à null l'état de l'utilisateur
+               setUser(null); 
           }
       });
-
-      // Arrêter l'écoute des changements d'état de l'utilisateur lors du démontage du composant
       return () => unsubscribe();
   }, []);
 
-  // Extraire le prénom de l'utilisateur
+
   const firstName = user ? user.displayName.split(' ')[0] : '';
 
   return (
